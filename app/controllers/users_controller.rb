c03f3420e1f0303a092c7b530b219a727f371bcb
Hao Sun <hao.sun@nyu.edu>
2017-09-09 16:52:49 -0400

    add controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:show, :edit, :update, :destroy]
+    
     def index
      @users = User.all
     end
@@ -7,23 +9,53 @@ class UsersController < ApplicationController
      @user = User.find(params[:id])
     end   
 
-    # def create
-    # user = params['user']
-    # User.create(name: user['name'],
-    #             email: user['email'])
-    # redirect_to("/users")            
-    # end
+    def new
+    @user = User.new
+    end
 
-    # def destroy
-    # User.destroy(params['id'])
-    # redirect_to("/users")
-    # end
+    def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to '/users'
+    else
+      render_to '/users/new'
+    end 
+    end
 
-    # def update
-    # user = params['user']   
-    # User.update(name: user['name'],
-    #             email: user['email'])
-    # redirect_to("/user")  
-    # end
+#     def edit
+#     end
+
+#     def update
+#     if @user.update(user_params)
+#       redirect_to @user
+#     else
+#       render 'edit'
+#     end
+#   end
+
+  def destroy
+    @user.destroy
+    redirect_to '/users'
+  end
+
+   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      redirect_back fallback_location: edit_user_path(@user)
+    end
+  end
+
+    def user_params
+    params.require(:user).permit(:name, :email)
+   end
+
+    def set_user
+    @user = User.find(params[:id])
+    end
 end
 
